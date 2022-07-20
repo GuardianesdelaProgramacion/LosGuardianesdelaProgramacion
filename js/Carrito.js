b05af016a4c0aff06e46101d421fa42c0d52142c
Saul Del Carmen <saul.delcarmenhernandez@gmail.com>
2022-07-20 13:18:16 -0500

    Add: Carritojs

--- js/Carrito.js
+++ js/Carrito.js
@@ -3,3 +3,9 @@ console.log("Carrito.js");
 function limpiarTabla(){
 	document.getElementById('data').innerHTML = '';
 }
+
+
+function Eliminar (i) {
+ document.getElementsByTagName("table")[0].setAttribute("id","tableid");
+ document.getElementById("tableid").deleteRow(i);
+}
