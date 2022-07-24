399d3e83cd38aed12f13cb5eba36ce4a300be64c
Saul Del Carmen <saul.delcarmenhernandez@gmail.com>
2022-07-24 03:37:57 -0500

    Add: Carrito empty view/hidden popup

--- js/Carrito.js
+++ js/Carrito.js
@@ -4,8 +4,6 @@ function limpiarTabla(){
 	document.getElementById('data').innerHTML = '';
 }
 
-
-function Eliminar (i) {
- document.getElementsByTagName("table")[0].setAttribute("id","tableid");
- document.getElementById("tableid").deleteRow(i);
+function eliminarFila(index) {
+  $("#fila" + index).remove();
 }
