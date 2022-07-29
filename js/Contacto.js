54284824b3c39938341fa8d76c1f553c6c6cba88
Pablo Lopez <pablom.lopez.garcia@gmail.com>
2022-07-28 20:36:03 -0500

    Fix:popup contacto

--- js/Contacto.js
+++ js/Contacto.js
@@ -31,14 +31,32 @@ function contactoEnviar(){
 
     contactoData.push(objL);
     console.log(contactoData);
+
+    /*popup onclick*/
     popup1.style.visibility = "visible";
     popup1.style.opacity= 1;
+    cn1.style.visibility = "hidden";
+    cn2.style.visibility = "hidden";
+    cn3.style.visibility = "hidden";
+    cn4.style.visibility = "hidden";
+    cn5.style.visibility = "hidden";
 
 
 }
 
+/*Clode del popup*/
 function cnClose(){
     popup1.style.visibility = "hidden";
     popup1.style.opacity= 0;
+    cn1.style.visibility = "visible";
+    cn2.style.visibility = "visible";
+    cn3.style.visibility = "visible";
+    cn4.style.visibility = "visible";
+    cn5.style.visibility = "visible";
+    document.getElementById("cn1").value = "";
+    document.getElementById("cn2").value = "";
+    document.getElementById("cn3").value = "";
+    document.getElementById("cn4").value = "";
+    document.getElementById("cn5").value = "";
 
 }
