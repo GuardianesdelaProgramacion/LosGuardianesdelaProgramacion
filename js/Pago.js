7276e53fd09349f2b990e7f02b0996dfe7eca66c
Lucia Cortes <luciacortesmolina@gmail.com>
2022-08-06 21:03:01 -0500

    Fix:Pago

--- js/Pago.js
+++ js/Pago.js
@@ -3,17 +3,20 @@
 const formulario = document.querySelector('#formularioTarjeta');
 const numeroTarjeta = document.querySelector('#datosTarjeta #inputNumero');
 const nombreTarjeta = document.querySelector('#datosTarjeta #inputNombre');
-const mesExpiracion = document.querySelector('#datosTarjeta .grupo-expira #selectMes');
-const yearExpiracion = document.querySelector('#datosTarjeta .grupo-expira #selectYear');
-const ccv = document.querySelector('#datosTarjeta #grupo-expira .inputCCV');
+const mesExpiracion = document.querySelector('#selectMes');
+const yearExpiracion = document.querySelector('#selectYear');
+const ccv = document.querySelector('#inputCCV');
 
-const datosEnvio = document.querySelector('#dirEnvio');
-const nombreRecibe = document.querySelector('.grupoPersonaRecibe #Nombre');
-const apellidoRecibe = document.querySelector('.grupoPersonaRecibe #Apellido');
-const codigoPostal = document.querySelector('.grupoDireccion #CP');
-const telefono = document.querySelector('.grupoDireccion #Tel');
 
-closePago();
+const envio = document.querySelector('#envio');
+console.log(envio);
+const nombreEnvio = document.querySelector('#envio #nombreEnvio');
+const apellidoEnvio = document.querySelector('#envio #apellidoEnvio');
+const codigoPostal = document.querySelector('#envio .grupoDireccion #codigoPostal');
+const telefono = document.querySelector('#envio .grupoDireccion #telefono');
+
+
+// //closePago();
 
 
 /* Select del mes generado dinámicamente */
@@ -55,28 +58,28 @@ formulario.inputNombre.addEventListener('keyup', (e) => {
     formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
 })
 //CCV
-formulario.inputCCV.addEventListener('keyup', () => {
+formulario.inputCCV.addEventListener('keyup', (e) => {
     formulario.inputCCV.value = formulario.inputCCV.value
         .replace(/\s/g, '')
         .replace(/\D/g, '');
-})
-/**Input Nombre y Apellidos de quien recibe**/
-datosEnvio.Nombre.addEventListener('keyup', (e) => {
-    let valorInput = e.target.value
-    datosEnvio.Nombre.value = valorInput.replace(/[0-9]/g, '');
 });
-datosEnvio.Apellido.addEventListener('keyup', (e) => {
+/**Input Nombre y Apellidos de quien recibe**/
+datosEnvio.nombreEnvio.addEventListener('keyup', (e) => {
     let valorInput = e.target.value
-    datosEnvio.Apellido.value = valorInput.replace(/[0-9]/g, '');
-})
+    datosEnvio.nombreEnvio.value = valorInput.replace(/[0-9]/g, '');
+});
+datosEnvio.apellidoEnvio.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value
+    datosEnvio.apellidoEnvio.value = valorInput.replace(/[0-9]/g, '');
+});
 /**Input del Código Postal y de teléfono */
-datosEnvio.CP.addEventListener('keyup', () => {
-    datosEnvio.CP.value = datosEnvio.CP.value
+datosEnvio.codigoPostal.addEventListener('keyup', () => {
+    datosEnvio.codigoPostal.value = datosEnvio.codigoPostal.value
         .replace(/\s/g, '')
         .replace(/\D/g, '');
-})
-datosEnvio.Tel.addEventListener('keyup', () => {
-    datosEnvio.Tel.value = datosEnvio.Tel.value
+});
+datosEnvio.telefono.addEventListener('keyup', () => {
+    datosEnvio.telefono.value = datosEnvio.telefono.value
         .replace(/\s/g, '')
         .replace(/\D/g, '')
         //Ponemos espacio cada cuatro números
@@ -84,36 +87,36 @@ datosEnvio.Tel.addEventListener('keyup', () => {
         //Quitamos el último espaciado
         .trim();
 })
-function terminarPedido() {
-    /*popup onclick*/
-    popup2.style.visibility = "visible";
-    popup2.style.opacity = 1;
-    pay1.style.visibility = "hidden";
-    Nombre.style.visibility = "hidden";
-    Apellido.style.visibility = "hidden";
-    pay4.style.visibility = "hidden";
-    pay5.style.visibility = "hidden";
-    pay6.style.visibility = "hidden";
-    CP.style.visibility = "hidden";
-    Tel.style.visibility = "hidden";
-    inputNumero.style.visibility = "hidden";
-    inputNombre.style.visibility = "hidden";
-    inputCCV.style.visibility = "hidden";
-}
-/*Close del popup*/
-function closePago() {
-    popup2.style.visibility = "hidden";
-    popup2.style.opacity = 0;
-    pay1.style.visibility = "visible";
-    Nombre.style.visibility = "visible";
-    Apellido.style.visibility = "visible";
-    pay4.style.visibility = "visible";
-    pay5.style.visibility = "visible";
-    pay6.style.visibility = "visible";
-    CP.style.visibility = "visible";
-    Tel.style.visibility = "visible";
-    inputNombre.style.visibility = "visible";
-    inputNumero.style.visibility = "visible";
-    inputCCV.style.visibility = "visible";
-    document.getElementById("posicion").value = "";
-}
+// function terminarPedido() {
+//     /*popup onclick*/
+//     popup2.style.visibility = "visible";
+//     popup2.style.opacity = 1;
+//     pay1.style.visibility = "hidden";
+//     Nombre.style.visibility = "hidden";
+//     Apellido.style.visibility = "hidden";
+//     pay4.style.visibility = "hidden";
+//     pay5.style.visibility = "hidden";
+//     pay6.style.visibility = "hidden";
+//     CP.style.visibility = "hidden";
+//     Tel.style.visibility = "hidden";
+//     inputNumero.style.visibility = "hidden";
+//     inputNombre.style.visibility = "hidden";
+//     inputCCV.style.visibility = "hidden";
+// }
+// /*Close del popup*/
+// function closePago() {
+//     popup2.style.visibility = "hidden";
+//     popup2.style.opacity = 0;
+//     pay1.style.visibility = "visible";
+//     Nombre.style.visibility = "visible";
+//     Apellido.style.visibility = "visible";
+//     pay4.style.visibility = "visible";
+//     pay5.style.visibility = "visible";
+//     pay6.style.visibility = "visible";
+//     CP.style.visibility = "visible";
+//     Tel.style.visibility = "visible";
+//     inputNombre.style.visibility = "visible";
+//     inputNumero.style.visibility = "visible";
+//     inputCCV.style.visibility = "visible";
+//     document.getElementById("posicion").value = "";
+// }
