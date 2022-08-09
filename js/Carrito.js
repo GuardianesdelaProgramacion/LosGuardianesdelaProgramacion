84144f66fa5b30cf8acb67ae839f7c4b7ff82d3d
Saul Del Carmen <saul.delcarmenhernandez@gmail.com>
2022-08-09 16:36:07 -0500

    Mod: Nosotros

--- js/Carrito.js
+++ js/Carrito.js
@@ -12,8 +12,6 @@ function limpiarTabla2(){
 function eliminarFila(index) {
   $("#fila" + index).remove();
 }
-const Clickbutton2 = document.querySelectorAll('button');
-console.log(Clickbutton2);
 
 
 
@@ -22,20 +20,100 @@ console.log(Clickbutton2);
 
 Obtenerdatoss();
 function Obtenerdatoss() {
-    if ( localStorage.getItem("1") !== null   ) {
-      alert("Sidebar si existe en localStorage!!");
-    let nombre=localStorage.getItem("10");
-    console.log(nombre);
-    nombre=JSON.parse(nombre);
+    if ( localStorage.getItem("carrito") !== null   ) {
+  //    alert("Sidebar si existe en localStorage!!");
+    let compra=localStorage.getItem("10");
+    console.log(compra);
+    //compra=JSON.parse(compra);
+   let datos = "";
+  for (let i = 1; i < localStorage.length + 1; i++) {
+    compra = JSON.parse(compra);
+    if (compra!==null) {
+      datos+= 
+        `<tr class="mytr" id="fila${lista.id}">
+        <td><button value="Eliminar" style="font-size:30px" class="bi bi-x buttonx" onclick="eliminarFila(${lista.id})"></button></td>
+<td><img class="img_carrito" src="${lista.url}"alt="Producto 1" /></td>
+<td>${lista.descripción}</td>
+        <td><span>${lista.precio}<span></td>
+        <td><input class="input_carrito" type="number" min="1" max="${lista.id}" value=${lista.id}></td>
+        <td>$<span>60.00<span></td>
+    </tr>
+      `;
+    }
+  }
+  document.getElementById(data).innerHTML = datos;
+
+
+
   }
 else {
-  alert("No hay localStorage!!");
+ // alert("No hay localStorage!!");
     limpiarTabla();
     limpiarTabla2();
 }
 
 }
 
+
+ function carritoaPrincipal(compra) {
+  let datos = "";
+  for (let i = 1; i < localStorage.length + 1; i++) {
+    lista = JSON.parse(localStorage.getItem(i));
+    if (lista.seccion == "catalogo") {
+      datos+= 
+        `<tr class="mytr" id="fila${lista.id}">
+        <td><button value="Eliminar" style="font-size:30px" class="bi bi-x buttonx" onclick="eliminarFila(${lista.id})"></button></td>
+<td><img class="img_carrito" src="${lista.url}"alt="Producto 1" /></td>
+<td>${lista.descripción}</td>
+        <td><span>${lista.precio}<span></td>
+        <td><input class="input_carrito" type="number" min="1" max="${lista.id}" value=${lista.id}></td>
+        <td>$<span>60.00<span></td>
+    </tr>
+      `;
+    }
+  }
+  document.getElementById(dataa).innerHTML = datos;
+}
+
+ function vendido(data2a) {
+  let datos = "";
+  let o=1;
+  for (let i = 1; i < localStorage.length + 1; i++) {
+    lista = JSON.parse(localStorage.getItem(i));
+    if (lista.seccion == "catalogo") {
+      datos+= 
+        `<tr class="mytr" id="fila${lista.id}">
+        <td><button value="Eliminar" style="font-size:20px;background-color:whith;" class="bi bi-x buttonx" onclick="eliminarFila(${lista.id})"></button></td>
+        <td><img class="img_carrito2" src="${lista.url}"alt="Producto 1" /></td>
+        <td><p style="font-size:10px">${lista.descripción}</p></td>
+        <td><input class="input_carrito" type="number" min="1" max="${lista.id}" value=${lista.id}></td>
+    </tr>
+      `;
+    }
+  }
+  document.getElementById(data2a).innerHTML = datos;
+}
+
+    let compra=localStorage.getItem("10");
+    console.log(compra);
+    //compra=JSON.parse(compra);
+   let datos = "";
+  for (let i = 1; i < localStorage.length + 1; i++) {
+    compra = JSON.parse(compra);
+    if (compra!==null) {
+      datos+= 
+        `<tr class="mytr" id="fila${lista.id}">
+        <td><button value="Eliminar" style="font-size:30px" class="bi bi-x buttonx" onclick="eliminarFila(${lista.id})"></button></td>
+<td><img class="img_carrito" src="${lista.url}"alt="Producto 1" /></td>
+<td>${lista.descripción}</td>
+        <td><span>${lista.precio}<span></td>
+        <td><input class="input_carrito" type="number" min="1" max="${lista.id}" value=${lista.id}></td>
+        <td>$<span>60.00<span></td>
+    </tr>
+      `;
+    }
+  }
+  document.getElementById(data).innerHTML = datos;
 
 
 
@@ -62,283 +140,7 @@ document.getElementById('dat').innerHTML = body;
  }
 }
 */
-function apiAnt(){
-     apianterior = localStorage.getItem('data1');
-	 console.log(apianterior)
-}
 
 
 
-
-
-
-// GET request for remote image in node.js codigo de sergio
-async function adquirirDatos(proveedor = "Axios", direccionhttp) {
-  if (proveedor == "Axios") {
-    return new Promise((resolve, reject) => {
-      axios({
-        method: "get",
-        url: direccionhttp,
-        responseType: "stream",
-      })
-        .then((usuarios) => {
-          //console.log("Axios: "+JSON.stringify(usuarios));
-          resolve(usuarios.data.data);
-        })
-        .catch((error) => {
-          console.log(error);
-          reject(error);
-        });
-    });
-  } else if (proveedor == "Fetch") {
-    return new Promise((resolve, reject) => {
-      fetch(direccionhttp)
-        .then((responseJSON) => responseJSON.json())
-        .then((usuarios) => {
-          //console.log("Fetch: "+JSON.stringify(usuarios));
-          resolve(usuarios.data)
-        })
-        .catch((error) => {
-          console.log(error);
-          reject(error);
-        });
-    });
-  }
-  else {
-    return new Promise((resolve, reject) => {
-      fetch(direccionhttp)
-        .then((responseJSON) => responseJSON.json())
-        .then((usuarios) => {
-          //console.log("Json: "+JSON.stringify(usuarios));
-          resolve(usuarios.data.data)
-        }
-        )
-        .catch((error) => {
-          console.log(error);
-          reject(error);
-        });
-    });
-  }
-}
-
-solicitudBtn();
-async function solicitudBtn() {
-  // datos = await adquirirDatos("Axios", "https://reqres.in/api/users?delay=3");
-  // console.log("Solicitud Axios:" + JSON.stringify(datos));
-  // datos = await adquirirDatos("Fetch", "https://reqres.in/api/users?delay=3");
-  // console.log("Solicitud Fetch:" + JSON.stringify(datos));
-  datos = await adquirirDatos("Json", "../assets/json2/productos.json");
-  console.log("Solicitud Json Productos:" + JSON.stringify(datos));
-  masVendido = await adquirirDatos("Json", "../assets/json2/masVendido.json");
-  console.log("Solicitud Json Mas Vendido:" + JSON.stringify(datos));
-
-
-  for (let user of datos) {
-
-    let llave = JSON.stringify(user.id)
-    localStorage.setItem(llave, JSON.stringify(user));
-  }
-
-  for (let user of masVendido) {
-    let llave = JSON.stringify(user.id)
-    localStorage.setItem(llave, JSON.stringify(user));
-  }
-
-  catalogosProductos("data");
-  vendido("data2");
-
-}
-
-
-
-
-
-//Función para mostrar los productos de forma dinamica 
- function catalogosProductos(data) {
-  let datos = "";
-  for (let i = 1; i < localStorage.length + 1; i++) {
-    lista = JSON.parse(localStorage.getItem(i));
-    if (lista.seccion == "catalogo") {
-      datos+= 
-        `<tr class="mytr" id="fila${lista.id}">
-        <td><button value="Eliminar" style="font-size:30px" class="bi bi-x buttonx" onclick="eliminarFila(${lista.id})"></button></td>
-<td><img class="img_carrito" src="${lista.url}"alt="Producto 1" /></td>
-<td>${lista.descripción}</td>
-        <td><span>${lista.precio}<span></td>
-        <td><input class="input_carrito" type="number" min="1" max="${lista.id}" value=${lista.id}></td>
-        <td>$<span>60.00<span></td>
-    </tr>
-      `;
-    }
-  }
-  document.getElementById(data).innerHTML = datos;
-}
-
- function vendido(data2) {
-  let datos = "";
-  let o=1;
-  for (let i = 1; i < localStorage.length + 1; i++) {
-    lista = JSON.parse(localStorage.getItem(i));
-    if (lista.seccion == "catalogo") {
-      datos+= 
-        `<tr class="mytr" id="fila${lista.id}">
-        <td><button value="Eliminar" style="font-size:20px;background-color:whith;" class="bi bi-x buttonx" onclick="eliminarFila(${lista.id})"></button></td>
-        <td><img class="img_carrito2" src="${lista.url}"alt="Producto 1" /></td>
-        <td><p style="font-size:10px">${lista.descripción}</p></td>
-        <td><input class="input_carrito" type="number" min="1" max="${lista.id}" value=${lista.id}></td>
-    </tr>
-      `;
-    }
-  }
-  document.getElementById(data2).innerHTML = datos;
-}
-
-
-
-
-
-
-
-//Función para agregar un producto al carrito
-
-
-
-const Clickbutton = document.querySelectorAll('.button')
-const tbody = document.querySelector('.tbody')
-let carrito = []
-
-Clickbutton.forEach(btn => {
-  btn.addEventListener('click', addToCarritoItem)
-})
-
-
-function addToCarritoItem(e){
-  const button = e.target
-  const item = button.closest('.mytr')
-  const itemTitle = item.querySelector('.producto-nombre').textContent;
-  const itemPrice = item.querySelector('.producto-precio').textContent;
-  const itemImg = item.querySelector('.img-producto').src;
-  console.log("asda");  
-  const newItem = {
-    title: itemTitle,
-    precio: itemPrice,
-    img: itemImg,
-    cantidad: 1
-  }
-
-  addItemCarrito(newItem)
-}
-
-
-function addItemCarrito(newItem){
-
-  const alert = document.querySelector('.alert')
-
-  setTimeout( function(){
-    alert.classList.add('hide')
-  }, 2000)
-    alert.classList.remove('hide')
-
-  const InputElemnto = tbody.getElementsByClassName('input__elemento')
-  for(let i =0; i < carrito.length ; i++){
-    if(carrito[i].title.trim() === newItem.title.trim()){
-      carrito[i].cantidad ++;
-      const inputValue = InputElemnto[i]
-      inputValue.value++;
-      CarritoTotal()
-      return null;
-    }
-  }
   
-  carrito.push(newItem)
-  
-  renderCarrito()
-} 
-
-
-function renderCarrito(){
-  tbody.innerHTML = ''
-  carrito.map(item => {
-    const tr = document.createElement('tr')
-    tr.classList.add('ItemCarrito')
-    const Content = `
-    
-    <th scope="row">1</th>
-            <td class="table__productos">
-              <img src=${item.img}  alt="">
-              <h6 class="title">${item.title}</h6>
-            </td>
-            <td class="table__price"><p>${item.precio}</p></td>
-            <td class="table__cantidad">
-              <input type="number" min="1" value=${item.cantidad} class="input__elemento">
-              <button class="delete btn btn-danger">x</button>
-            </td>
-    
-    `
-    tr.innerHTML = Content;
-    tbody.append(tr)
-
-    tr.querySelector(".delete").addEventListener('click', removeItemCarrito)
-    tr.querySelector(".input__elemento").addEventListener('change', sumaCantidad)
-  })
-  CarritoTotal()
-}
-
-function CarritoTotal(){
-  let Total = 0;
-  const itemCartTotal = document.querySelector('.itemCartTotal')
-  carrito.forEach((item) => {
-    const precio = Number(item.precio.replace("$", ''))
-    Total = Total + precio*item.cantidad
-  })
-
-  itemCartTotal.innerHTML = `Total $${Total}`
-  addLocalStorage()
-}
-
-function removeItemCarrito(e){
-  const buttonDelete = e.target
-  const tr = buttonDelete.closest(".ItemCarrito")
-  const title = tr.querySelector('.title').textContent;
-  for(let i=0; i<carrito.length ; i++){
-
-    if(carrito[i].title.trim() === title.trim()){
-      carrito.splice(i, 1)
-    }
-  }
-
-  const alert = document.querySelector('.remove')
-
-  setTimeout( function(){
-    alert.classList.add('remove')
-  }, 2000)
-    alert.classList.remove('remove')
-
-  tr.remove()
-  CarritoTotal()
-}
-
-function sumaCantidad(e){
-  const sumaInput  = e.target
-  const tr = sumaInput.closest(".ItemCarrito")
-  const title = tr.querySelector('.title').textContent;
-  carrito.forEach(item => {
-    if(item.title.trim() === title){
-      sumaInput.value < 1 ?  (sumaInput.value = 1) : sumaInput.value;
-      item.cantidad = sumaInput.value;
-      CarritoTotal()
-    }
-  })
-}
-
-function addLocalStorage(){
-  localStorage.setItem('carrito', JSON.stringify(carrito))
-}
-
-window.onload = function(){
-  const storage = JSON.parse(localStorage.getItem('carrito'));
-  if(storage){
-    carrito = storage;
-    renderCarrito()
-  }
-}
